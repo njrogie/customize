26586f46f4ca0b28d019c3396f0cbe02ce67962f
nrogiedc <nrogie@detectachem.com>
2022-12-29 23:14:15 -0500

    Removed golang, added kubernetes

--- ubuntu-init.sh
+++ ubuntu-init.sh
@@ -2,7 +2,17 @@
 
 sudo apt update -y
 sudo apt upgrade -y
+
+# install vim if possible
 sudo apt install vim
-sudo rm -rf /usr/local/go
-# https://github.com/canha/golang-tools-install-script
-wget -q -O - https://git.io/vQhTU | bash
+
+# install minikube and kubernetes
+
+sudo apt install qemu-system libvirt-daemon-system
+sudo usermod -a -G libvirt nrogie
+
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
+rm minikube-linux-amd64
+
+
