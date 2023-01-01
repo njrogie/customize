1e8695d84a6dd57622c0bd3bd68c06d81af2a8f5
nrogiedc <nrogie@detectachem.com>
2023-01-01 15:46:39 -0600

    added docker to the init file

--- ubuntu-init.sh
+++ ubuntu-init.sh
@@ -6,10 +6,20 @@ sudo apt upgrade -y
 # install vim if possible
 sudo apt install vim
 
-# install minikube and kubernetes
+# install docker -- https://docs.docker.com/engine/install/debian/
+sudo apt remove docker docker-engine docker.io containerd runc
+sudo apt install -y ca-certificates curl gnupg lsb-release
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update -y
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
-sudo apt install qemu-system libvirt-daemon-system
-sudo usermod -a -G libvirt nrogie
+# install minikube and kubernetes
+sudo apt install -y qemu-system libvirt-daemon-system
+sudo usermod -a -G libvirt $USER 
 
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
